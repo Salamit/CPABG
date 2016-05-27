4e3ceba5bb9a88dab95e2ba2ff903dcd1cfb0b51
Salami, Tope <salami.tope@gmail.com>
2016-05-27 22:04:01 +0900

    Chp 3 exercise 2

---
+++ 3ex2.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+main()
+{
+	float a, b;
+
+	printf("How much of a bonus did you get? ");
+	scanf(" %f", &a);
+
+	b = .85 * a;
+	  printf("If you give 15 percent to charity, you will still ");
+	  printf("have %.2f.", b);
+	  return 0;
+}
